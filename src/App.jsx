e5219a980ff23ed3de5d8b746af02203fc92d9bd
git <marlonergondossantos@gmail.com>
2019-11-04 13:51:09 -0200

    import GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import GlobalStyle from "./style/globalStyle";
+
 export default function app() {
   return (
     <>
+      <GlobalStyle />
       <h1>funcionando</h1>
     </>
   );
