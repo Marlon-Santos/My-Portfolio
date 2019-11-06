c422ad7c35226276a85a12adf2814a81dd46b025
git <marlonergondossantos@gmail.com>
2019-11-06 04:30:48 -0200

    clean code

---
+++ src/screen/Header/header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Container } from "./style";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <>
+        <Container></Container>
+      </>
+    );
+  }
+}
